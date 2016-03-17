d5235e3610c4f1dbfff348e63adacb0131f7a697
gilsondev <me@gilsondev.in>
2016-03-17 17:00:56 -0300

    Organizando estrutura do projeto

--- api/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from flask.ext.sqlalchemy import SQLAlchemy
-
-# Criando a instancia de SQLAlchemy
-db = SQLAlchemy()
-
-# Importando models para reconhecimento do Alembic
-from api.models import * # NOQA
--- manage.py
+++ manage.py
@@ -1,30 +1,10 @@
-# -*- coding: utf-8 -*-
-
+#!/usr/bin/env python
 import os
-from api import create_api, db
-from flask.ext.script import Manager, Shell
-from flask.ext.migrate import Migrate, MigrateCommand
+import sys
 
-api = create_api(os.getenv('FLASK_CONFIG') or 'default')
-manager = Manager(api)
-migrate = Migrate(api, db)
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "octofiles.settings")
 
+    from django.core.management import execute_from_command_line
 
-def make_shell_context():
-    """Insere as instancias da aplicacao e banco no contexto do shell"""
-    return {
-        'api': api,
-        'db': db
-    }
-manager.add_command("shell", Shell(make_context=make_shell_context))
-manager.add_command("db", MigrateCommand)
-
-
-@manager.command
-def test():
-    """Comando para execucao dos testes do projeto"""
-    import pytest
-    pytest.main("-s tests")
-
-if __name__ == '__main__':
-    manager.run()
+    execute_from_command_line(sys.argv)
